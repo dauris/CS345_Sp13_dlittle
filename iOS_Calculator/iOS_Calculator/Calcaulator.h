755b471f29355585aec2e952eb9bb6f616c732ac
Dauris Little <dlittle@Mac110-Simic.local>
2013-02-20 17:39:54 -0500

    Added Calculator class

---
+++ iOS_Calculator/iOS_Calculator/Calcaulator.h
@@ -0,0 +1,13 @@
+//
+//  Calcaulator.h
+//  iOS_Calculator
+//
+//  Created by Dauris Little on 2/20/13.
+//  Copyright (c) 2013 Dauris Little. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Calcaulator : NSObject
+
+@end
---
+++ iOS_Calculator/iOS_Calculator/Calcaulator.m
@@ -0,0 +1,13 @@
+//
+//  Calcaulator.m
+//  iOS_Calculator
+//
+//  Created by Dauris Little on 2/20/13.
+//  Copyright (c) 2013 Dauris Little. All rights reserved.
+//
+
+#import "Calcaulator.h"
+
+@implementation Calcaulator
+
+@end
