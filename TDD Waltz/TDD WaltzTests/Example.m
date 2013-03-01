e3210371cbf56420a0e9e3dbc862a78dfebff789
Dauris Little <dlittle@Mac110-Simic.local>
2013-03-01 10:43:39 -0500

    Test 2 Pass

--- TDD Waltz/TDD WaltzTests/Example.m
+++ TDD Waltz/TDD WaltzTests/Example.m
@@ -4,14 +4,23 @@
 
 #import "Example.h"
 
-@implementation Example
+@implementation Example {
+    
+    NSUserDefaults* _userDefaults;
+}
 
 -(id)initWithUserDefauts:(NSUserDefaults *)userDefault {
+    
     self =  [self init];
+    if (self) {
+        _userDefaults = userDefault;
+    }
     return self;
 }
 
 -(NSNumber*)nextReminderId {
+    
+    [_userDefaults setObject:@0 forKey:@"currentReminderId"];
     return @0;
 }
 @end
